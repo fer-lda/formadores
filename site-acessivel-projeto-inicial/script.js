f01e0eb98c9549d21a6b559cc61b5f2ef90b3e0e
fer-lda <admloanda@hotmail.com>
2025-04-08 15:32:25 -0300

    Add files via upload

--- site-acessivel-projeto-inicial/script.js
+++ site-acessivel-projeto-inicial/script.js
@@ -2,9 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = localStorage.getItem('tamanhoFonte') ? parseFloat(localStorage.getItem('tamanhoFonte')) : 1;
     const tamanhoMin = 0.8;
     const tamanhoMax = 2;
+    const botaoAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesAcessibilidade=document.getElementById('opcoes-acessibilidade');
+    botaoAcessibilidade.addEventListener('click', function () {
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista');
 
+    })
+    
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
     const aumentarFonteBotao = document.getElementById('aumentar-fonte');
     aumentarFonteBotao.addEventListener('click', function () {
         if (tamanhoAtualFonte < tamanhoMax) {
@@ -22,13 +28,20 @@ document.addEventListener('DOMContentLoaded', function () {
             localStorage.setItem('tamanhoFonte', tamanhoAtualFonte);
         }
     });
+    const alternaContraste = document.getElementById('alterna-contraste')
+    alternaContraste.addEventListener('click', function ()
+        {
+        document.body.classList.toggle('alto-contraste')
+        })
 
     // Lógica para alternar a visibilidade das opções de acessibilidade
-    const botaoAcessibilidade = document.getElementById('botao-acessibilidade');
-    const menuAcessibilidade = document.getElementById('acessibilidade');
+    /*const botaoAcessibilidade = document.getElementById('botao-acessibilidade');*/
+    /*const menuAcessibilidade = document.getElementById('acessibilidade');*/
+    /*const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade’);*/
 
-    botaoAcessibilidade.addEventListener('click', function () {
+    /*botaoAcessibilidade.addEventListener('click', function () {*/
         // Alterna a classe "show-options" para mostrar ou esconder as opções
-        menuAcessibilidade.classList.toggle('show-options');
-    });
+        /*menuAcessibilidade.classList.toggle('show-options');*/
+
+    
 });
